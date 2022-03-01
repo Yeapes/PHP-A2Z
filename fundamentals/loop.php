cc981bcf1de9ea33cd57fc52737faa6672b0ca88
Yeapes <yeapesh2831@gmail.com>
2022-03-02 00:08:39 +0600

    Multiple steping in for loop

--- fundamentals/loop.php
+++ fundamentals/loop.php
@@ -23,3 +23,14 @@ do{
     $i++;
     echo $i.PHP_EOL;
 }while($i < 5);
+
+
+
+
+//Multiple steping in for loop
+
+//print 1 - 10 & 10  - 1 
+
+for($i = 10, $j = 1; $i > 0; $i--, $j++){
+    echo $i.": $j"."\n";
+}
