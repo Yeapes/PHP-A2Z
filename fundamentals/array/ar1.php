1b44d29de759b329ae37c36af8f47438a4b0c10b
Yeapes <yeapesh2831@gmail.com>
2022-03-10 00:37:00 +0600

    Array basic

---
+++ fundamentals/array/ar1.php
@@ -0,0 +1,14 @@
+<?php
+
+$data = array(
+    "Rokib",
+    123,
+    12.2
+);
+
+//Count how many elements are in the array
+$count_total = count($data);
+
+for ($i = 0; $i < $count_total; $i++){
+    echo $data[$i]."\n";
+}
