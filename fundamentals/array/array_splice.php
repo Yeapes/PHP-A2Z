7963c3ed13f0d6921fc3f5440a03560afc168ec1
Yeapes <yeapesh2831@gmail.com>
2022-03-28 22:59:14 +0600

    Array splice added

---
+++ fundamentals/array/array_splice.php
@@ -0,0 +1,16 @@
+<?php
+
+$number = [12,14,18,20,26];
+
+//set true getting actual offset
+$slice_number = array_splice($number,1,-1,true);
+
+print_r($slice_number);
+print_r($number);
+
+$arr = ["name" => "Yeapes", "age" => "22","district" => "Noakhali"];
+$add_new = ["name" => "Raihan", "age" => "23"];
+$arr_slice = array_splice($arr,0,-1,$add_new,true);
+
+print_r($arr_slice);
+print_r($arr);
