4471ed01e07b817cb868788574141250411a30ef
Yeapes <yeapesh2831@gmail.com>
2022-03-30 22:21:42 +0600

    array splice updated

--- fundamentals/array/array_merge.php
+++ fundamentals/array/array_merge.php
@@ -7,3 +7,14 @@ $arr2 = ["Bangladesh University",22];
 $arr_combine = array_combine($arr1,$arr2);
 
 print_r($arr_combine);
+
+
+
+$random = ["method","token","charge_id","payment_id"];
+
+$r1 = array_slice($random,0,3);
+$r2 = array_slice($random,3);
+
+$r_merge = array_merge($r1,$r2);
+
+print_r($r_merge);
--- fundamentals/array/array_splice.php
+++ fundamentals/array/array_splice.php
@@ -8,6 +8,7 @@ $slice_number = array_splice($number,1,-1,true);
 print_r($slice_number);
 print_r($number);
 
+//Set new value to which is splice
 $arr = ["name" => "Yeapes", "age" => "22","district" => "Noakhali"];
 $add_new = ["name" => "Raihan", "age" => "23"];
 $arr_slice = array_splice($arr,0,-1,$add_new,true);
