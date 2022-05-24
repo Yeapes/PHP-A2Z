d07edc770beba41409d790ce9821484f9d15fbf5
Yeapes <yeapesh2831@gmail.com>
2022-05-24 23:32:05 +0600

    age dif

--- fundamentals/date/dif_time.php
+++ fundamentals/date/dif_time.php
@@ -8,3 +8,14 @@ $dt_two = mktime(0,0,0,10,30,2020);
 $dif_time = ($dt_one - $dt_two)/(24*60*60);
 
 echo $dif_time;
+
+
+
+//Get my age
+$dt_one = mktime(0,0,0,3,9,2000);
+
+$dt_two = mktime(0,0,0,5,24,2022);
+
+$dif_time = ($dt_two -  $dt_one )/(24*60*60)/365;
+
+echo $dif_time;
