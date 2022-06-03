6fe4e9c535f6e761eb7d3f96ca946e9bd5a575ca
Yeapes <yeapesh2831@gmail.com>
2022-06-03 11:46:57 +0600

    Update with Parent static access

--- oop_fundamentals/static_1.php
+++ oop_fundamentals/static_1.php
@@ -10,8 +10,25 @@ class ExampleStatic{
     public static function getGoal(){
         return self::$goal;
     }
+
+    public static function doSomething(){
+        echo "Do something from exampleStatic";
+    }
+
+}
+
+class DoSome extends ExampleStatic{
+    public static function doSomething()
+    {
+        echo "Do something from DoSome";
+        echo PHP_EOL;
+        parent::doSomething();
+    }
 }
 
 
 ExampleStatic::setGoal("I'm Planning to complete PHP OOP Advance in this month");
 echo ExampleStatic::getGoal();
+
+echo PHP_EOL;
+DoSome::doSomething();
