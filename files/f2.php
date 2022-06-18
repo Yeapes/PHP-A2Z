55b854319495d79c85f3fdcf0ffc5685fbf299b4
Yeapes <40892003+Yeapes@users.noreply.github.com>
2022-06-18 15:38:31 +0600

    Create f2.php
    dir function using for Directory class

---
+++ files/f2.php
@@ -0,0 +1,10 @@
+<?php
+
+//Return an instance from Directory class
+$d = dir("/etc/php5");
+echo "Handle: " . $d->handle . "\n";
+echo "Path: " . $d->path . "\n";
+while (false !== ($entry = $d->read())) {
+   echo $entry."\n";
+}
+$d->close();
