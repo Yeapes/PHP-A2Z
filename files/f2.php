c634ca0a3ae79fa3c115eb8bc4158ecfd1e61f0d
Yeapes <yeapesh2831@gmail.com>
2022-08-10 22:57:52 +0600

    pathinfo file function

---
+++ files/f2.php
@@ -0,0 +1,17 @@
+<?php
+
+$path = "C:/xampp/htdocs/PHP-A2Z/files/f1.php";
+
+echo pathinfo($path, PATHINFO_BASENAME);
+
+echo PHP_EOL;
+
+echo pathinfo($path, PATHINFO_EXTENSION);
+
+
+echo PHP_EOL;
+echo pathinfo($path, PATHINFO_EXTENSION);
+
+echo PHP_EOL;
+
+echo pathinfo($path, PATHINFO_FILENAME);
