ef924b24a17cc8315fe42795e8ad320b20e5598c
Yeapes <yeapesh2831@gmail.com>
2022-09-14 21:55:38 +0600

    Dependcy injection example with Create file and append data on existing file

--- oop_advance/filemange_dependency_injection.php
+++ oop_advance/filemange_dependency_injection.php
@@ -2,29 +2,43 @@
 
  interface BaseStorage{
 
-    function writeFile();
-    function appendFile();
+    function writeFile($data);
+    function setFileName($fn);
+    function setMode($mode);
  }
 
  class Storage implements BaseStorage{
 
     private $fn;
+    private $mode;
     
-    function __construct($fn)
+    function __construct($fn,$mode = null)
     {
-        $this->fn = $fn;
+        $this->setFileName($fn);
+        $this->mode = $mode;
     }
-    function writeFile()
+    function writeFile($data)
     {
-        
+        file_put_contents($this->fn,$data,$this->mode);
     }
 
-    function appendFile()
+   function setFileName($fn)
     {
-        
+      $this->fn = $fn;  
+    }
+
+    function setMode($mode){
+      $this->mode = $mode;
     }
  }
 
- class StorageManager{
-
+ class DataManager{
+      function saveData(BaseStorage $storage, $data){
+         $storage->writeFile($data);
+      }
  }
+
+$data = new DataManager();
+$storage = new Storage("C:/xampp/htdocs/PHP-A2Z/oop_advance/test.txt",FILE_APPEND);
+
+$data->saveData($storage, "This is a test data and then append a data");
